b76a478897a7c6e1588d96bcd3ca290c9910fd90
subhanje <subhanjee14@gmail.com>
2023-09-26 12:49:22 +0500

    done

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -7,6 +7,8 @@ import random1 from "../../images/Vector (22).png";
 import random2 from "../../images/image 15 (2).png";
 import random3 from "../../images/image 15 (3).png";
 import random4 from "../../images/image 14.png";
+import random5 from "../../images/Vector (28).png";
+import random6 from "../../images/image 10.png";
 function About() {
   return (
     <div className="main-about">
@@ -60,6 +62,7 @@ function About() {
                   <br /> sed do eiusmod tempor
                   <br /> incididunt ut
                 </p>
+                <img src={random5} alt="" className="pos-vec-roadmbl" />
               </div>
               <div className="div-pos-road1">
                 <p className="text-about-m">Q3 2023</p>
@@ -81,6 +84,7 @@ function About() {
                 </p>
                 <img src={random1} alt="" className="pos-vec-road" />
                 <img src={random3} alt="" className="pos-vec-road2" />
+                <img src={random6} alt="" className="pos-vec-roadmbl1" />
               </div>
               <div className="div-pos-road3">
                 <p className="text-about-m">Q3 2023</p>
